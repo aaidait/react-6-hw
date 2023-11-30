ea2bade0ec7ca34497d8d72e2e68e7357ad45bac
AIDAI <turdakunovaidai@gmail.com>
2023-11-30 20:42:06 +0600

    upload

--- routing-project/src/components/pages/AboutUs/AboutUs.jsx
+++ routing-project/src/components/pages/AboutUs/AboutUs.jsx
@@ -4,14 +4,36 @@ import './AboutUs.scss';
 const AboutUs = () => {
     return (
         <div>
-            <h2>About Us</h2>
+            <div>
+          <h2>About Us</h2>
             <p>From the mountains of Colorado to the beaches of California, and Portlands in Maine and Oregon,
                 to main streets across the country, there’s a Toad just about everywhere these days. But like most great things,
                 it all started with humble beginnings: just one woman in a garage in Telluride, CO making handmade toque hats for
                 her friends. It was 1991 and fleece hats were all the rage – right up there with REM singles and two-way pagers.
             </p>
+            <img src={img} alt="Toad&Co Logo" />
+            </div>
+            
+           
+
+            <section>
+                <p>1995: the first Horny Toad store opens in downtown Telluride, expanding the line to include fleece vests and jackets. While things are heating up in Telluride, we move our headquarters 
+                    to Chicago where three Toads operate the business out of the living room. One of our proudest moments comes in 1997 when we partner with Search, Inc. to co-found the Planet Access Company, 
+                    a logistics warehouse that provides career opportunities for adults with developmental disabilities. We kick things off with the Camping Pillow Program: Toad provides 
+                    fleece scraps from production and Search clients sew the scraps into camping pillowcases to be shipped to outdoor stores across the country. All proceeds from the pillowcases 
+                    go back to PAC to offer more career development opportunities and send crew members on much deserved vacations to the great outdoors.</p>
+            </section>
+            <img src={img2} alt="" />
+            <section>
+                <p>In 2015 we changed our name from Horny Toad to Toad&Co to better reflect our commitment to creating global 
+                    change through meaningful partnerships. From our manufacturers to our retailers, we’ve committed to finding 
+                    partners who work toward social and environmental progress. In the last 20 years, together we have helped to fund 
+                    120 environmental protection organizations, employed roughly 400 adults with disabilities, and sent 500 people on 
+                    once-in-a-lifetime trips to the great outdoors. And we’re just getting started.</p>
+            </section>
         </div>
     );
 };
+
 
 export default AboutUs;
--- routing-project/src/components/pages/Home/Home.jsx
+++ routing-project/src/components/pages/Home/Home.jsx
@@ -8,8 +8,27 @@ const Home = () => {
                 take care of the planet and empower people to live their fullest lives. These partnerships have shaped us
                 for the better, helping us to live well and do good every day.
             </p>
+            <section>
+                <h2>Choosing sustainability isn't the easy route, it's the only route.</h2>
+                <p>Choosing sustainability isn't the easy route, it's the only route. The apparel industry is 
+                    the fourth largest polluter of air and water on Earth. The solution? Go sustainable, or go nude.
+                    Minimizing our environmental impact and helping clean up the apparel industry is an 
+                    integral part of our business ethos. 
+                </p>
+            </section>
+            <h2>1% For the Planet</h2>
+            <p>is a global network of businesses, individuals, and nonprofits working to tackle 
+                our planet's most pressing environmental issues. As a member, we give 1% of all sales back to environmental non-profits 
+                and get our hands dirty regularly volunteering with organizations to restore trails, clean up our coastline and plant trees. 
+                We’re an active member in our local 1% for the Planet community and organize film festivals, environmental discussion panels
+                 and public outreach events to raise funds and awareness for our nonprofit partners.</p>
+                 <h2>The Conservation Alliance</h2>
+                 <p>partners with grassroots environmental organizations to protect the wild places we all love. 
+                    We joined The Conservation Alliance in 2006, making us a proud member for over a decade. Each year we participate in 
+                    The Conservation Alliance’s Backyard Collective, a day of local environmental activism, and every Toad gets to vote on grant 
+                    nominations each year to determine which conservation projects will receive funding. </p>
         </div>
     );
 };
-
+ 
 export default Home;
